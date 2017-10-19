5469a1f7586449c982656ca01a5cf8c3a28aa39d
Rumen Neshev <rstar2@abv.bg>
2017-10-20 00:05:01 +0300

    Add deacceleration and "breaking" to WalkTrait

--- public/js/traits/Walk.js
+++ public/js/traits/Walk.js
@@ -13,6 +13,7 @@ export default class WalkTrait extends Trait {
 
         // used if WalkTrait.IS_ACCELERATING is true
         this._acceleration = 400;
+        this._deacceleration = 300;
         this._dragFactor = 1 / 5000;
 
         // the distance "walked" when in single "walking" phase
@@ -66,16 +67,34 @@ export default class WalkTrait extends Trait {
             // this will be with incrementing speed - e.g. accelerating
             entity.vel.x += this._direction * (this._acceleration * rate);
 
-            // meassure the distance "walked" after the last stop
-            this._distance += Math.abs(entity.vel.x) * rate;
             // also remember where the entity is heading last
             this._lastDirection = this._direction;
+        } else if (entity.vel.x !== 0) {
+            // if stopped walking (e.g. this._direction is 0)
+            // but it's stil moving because of the acceleration
+            // then apply some DEacceleration that will stop him completely
+
+            // this will in fact speed up the drag
+            const deaccel = Math.min(Math.abs(entity.vel.x), this._deacceleration * rate);
+            entity.vel.x -= entity.vel.x > 0 ? deaccel : -deaccel;
         } else {
             this._distance = 0;
         }
 
-        const drag = this._dragFactor * entity.vel.x * Math.abs(entity.vel.x);
-        entity.vel.x -= drag;
+        if (entity.vel.x !== 0) {
+            // this will add some drag - the higher the velocity the grater the drag
+            // it will be always with the "sign +/-" s the velocity, so we'll be always
+            // subsctractig late from the velocity - so real drag
+
+            // note - put the drag 
+            // 1. while moving (e.g a slowing little by little and thus applying a max speed)
+            // 2. even if stopped walking (e.g. this._direction is 0) - this will allow to "slide" a little as it has some inercia 
+            const drag = this._dragFactor * entity.vel.x * Math.abs(entity.vel.x);
+            entity.vel.x -= drag;
+        }
+
+        // meassure the distance "walked" after the last stop
+        this._distance += Math.abs(entity.vel.x) * rate;
     }
 
     get direction() {
@@ -94,7 +113,20 @@ export default class WalkTrait extends Trait {
     }
 
     animate(entity, animation) {
-        let tile = this.distance ? animation(this.distance) : undefined;
+        // TODO: Don't hardcore the  'break' tile like that
+        // For this allow each trait to have multiple animations for fifferent purposes
+        // "run" -> ["run-1", "run-2", "run-3"]
+        // "break" -> ["break", ...]
+        let tile;
+        if (this.distance > 0) {
+            if ((entity.vel.x > 0 && this.direction < 0) ||
+                (entity.vel.x < 0 && this.direction > 0)) {
+                // when "breaking" - e.g. changing the direction
+                tile = 'break';
+            } else {
+                tile = animation(this.distance);
+            }
+        }
         const mirrored = this.heading < 0;
 
         return { tile, mirrored };
