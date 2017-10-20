f8b73cb3bfe137b6095ffc544f648425e42fd168
rstar2 <rstar2@abv.bg>
2017-10-20 17:44:21 +0300

    Allow Jump grace-time

--- public/js/traits/Jump.js
+++ public/js/traits/Jump.js
@@ -5,26 +5,42 @@ export default class JumpTrait extends Trait {
     constructor() {
         super('jump');
 
-        this._duration = 0.5;
+        this._duration = 1.3;
         this._velocity = 200;
         this._engagedTime = 0;
 
         // state to indicate whether it can jump - it can only from when touched a 'ground'
         // this means that there will be needed a 2 step-update in order to animate
         this._ready = 0;
+
+        // allow a grace time in which we can jump, e.g.
+        // next jump can occur while falling and start
+        // is pressed when very close to the ground (allow a grace period)
+        this._requestTime = 0;
+        this._gracePeriod = 1.5; // seconds
     }
 
     start() {
-        if (this._ready > 0) {
-            this._engagedTime = this._duration;
-        }
+        this._requestTime = this._gracePeriod;
     }
 
     cancel() {
         this._engagedTime = 0;
+        this._requestTime = 0;
     }
 
     update(entity, rate) {
+        // check if we've pressed to 'start'
+        // then we'll start to look for an opportunaty to jump
+        if (this._requestTime > 0) {
+            if (this._ready > 0) {
+                this._engagedTime = this._duration;
+                this._requestTime = 0;
+            } else {
+                this._requestTime -= rate;
+            }
+        }
+        
         if (this._engagedTime > 0) {
             // update just the 'y' coordinate
             entity.vel.y = -this._velocity;
