cb475369b6874962299f8691b89e970536dbff6c
rstar2 <rstar2@abv.bg>
2017-12-05 16:45:31 +0200

    Fix Electron support

---
+++ src/js-electron/renderer.js
@@ -0,0 +1,33 @@
+// This file is required by the electron.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+function fetchLocal(name) {
+    return new Promise(function (resolve, reject) {
+        const xhr = new XMLHttpRequest;
+        xhr.onload = function () {
+            resolve(new Response(xhr.responseText, { status: xhr.status }));
+        }
+        xhr.onerror = function () {
+            reject(new TypeError('Local request failed'));
+        };
+        xhr.open('GET', name);
+        xhr.send(null);
+    });
+}
+const fetchOrig = window.fetch;
+
+window.fetch = function (name) {
+    const url = require('url');
+
+    const fetchUrl = url.parse(name);
+    if (fetchUrl.protocol === 'file:') {
+        // requested name is local file
+        return fetchLocal(name);
+    } else if (!fetchUrl.protocol && url.parse(window.location.href).protocol === 'file:') {
+        // requested name is relative to a local file
+        return fetchLocal(name);
+    }
+
+    return fetchOrig(name);
+};
--- webpack.config.js
+++ webpack.config.js
@@ -6,78 +6,95 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const folderDist = 'public';
-const bundleJS = 'js/bundle.js';
+const bundleJS = 'js/bundle.app.js';
 const bundleSTYLES = 'css/bundle.css';
 
-module.exports = {
-    entry: [
-        './src/js/main.js',
+module.exports = function (env) {
+    const config = {
+        entry: {
+            [bundleJS]: [
+                './src/js/main.js',
 
-        // import CSS but actually it will be extracted to a separate combined file
-        './src/css/styles.css'
-    ],
+                // import CSS but actually it will be extracted to a separate combined file
+                './src/css/styles.css'
+            ]
+        },
 
-    output: {
-        filename: bundleJS,
-        path: path.resolve(__dirname, folderDist),
-    },
+        output: {
+            filename: '[name]',
+            path: path.resolve(__dirname, folderDist),
+        },
 
-    resolve: {
-        extensions: ['.js']
-    },
+        resolve: {
+            extensions: ['.js']
+        },
 
-    plugins: [
-        // clean the build folder first
-        new CleanWebpackPlugin(folderDist),
+        plugins: [
+            // clean the build folder first
+            new CleanWebpackPlugin(folderDist),
 
-        new ExtractTextPlugin(bundleSTYLES),
+            new ExtractTextPlugin(bundleSTYLES),
 
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            }),
 
-        new CopyWebpackPlugin([
-            { from: './src/img', to: 'img' },
-            { from: './src/data', to: 'data' }
-        ])
-    ],
+            new CopyWebpackPlugin([
+                { from: './src/img', to: 'img' },
+                { from: './src/data', to: 'data' }
+            ])
+        ],
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        "presets": [
-                            "es2015",
-                            "stage-0"
-                        ],
-                        "plugins": [
-                            "transform-runtime"
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: [
-                        {
-                            loader: "css-loader",
-                            options: {
-                                url: false, // don't import/inline the url like in background-image : url('')
-                                sourceMap: true
-                            }
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            "presets": [
+                                "es2015",
+                                "stage-0"
+                            ],
+                            "plugins": [
+                                "transform-runtime"
+                            ]
                         }
-                    ]
-                })
-            }
-        ]
-    },
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: "style-loader",
+                        use: [
+                            {
+                                loader: "css-loader",
+                                options: {
+                                    url: false, // don't import/inline the url like in background-image : url('')
+                                    sourceMap: true
+                                }
+                            }
+                        ]
+                    })
+                }
+            ]
+        },
 
-    // Turn on/off sourcemaps globally - for all loaders
-    devtool: 'source-map'
+        // Turn on/off sourcemaps globally - for all loaders
+        devtool: 'source-map'
+    };
+
+    // if building for Electron then add a new entry
+    if (env.electron) {
+        // This will build a new JS bundle but there's no guaranty
+        // that it will be put and parsed before the main JS bundle (but this is necessary)
+        // const bundleElectron = 'js/bundle.electron.js';
+        // config.entry[bundleElectron] = './src/js-electron/renderer.js';
+
+        // This will use the main JS bundle and add the Electron stuff in front
+        config.entry[bundleJS].unshift('./src/js-electron/renderer.js');
+    }
+
+    return config;
 };
